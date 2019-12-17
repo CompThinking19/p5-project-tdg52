52b4604446d976bcc458fa3658f8dd75f9514510
Trai <tdg52@scarletmail.rutgers.edu>
2019-12-17 11:37:12 -0500

    Put together the code for the project

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  // put setup code here
+  // Setting the background as a maze for the user to navigate
+  bg = loadImage('maze-clipart-line-art-2.png');
+  createCanvas(900, 913);
 }
 
+// Allowing the user to draw as they navigate to keep track of their movement
 function draw() {
-  // put drawing code here
+// Drawing a black line to make the mark as visible as possible
+  stroke(0);
+// Drawing through clicking and holding so the user can decide whether or not they want to make marks
+  if (mouseIsPressed === true) {
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
 }
