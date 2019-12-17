c5cc3a1d82a23756fbb3cfee8ea4a2e506072f74
Trai <tdg52@scarletmail.rutgers.edu>
2019-12-17 12:00:45 -0500

    Made some corrections to the code

--- sketch.js
+++ sketch.js
@@ -1,15 +1,23 @@
+// Preloading image that'll be set as the background
+let img;
+function preload() {
+  img = loadImage('maze-clipart-line-art-2.png');
+}
+
 function setup() {
-  // Setting the background as a maze for the user to navigate
-  bg = loadImage('maze-clipart-line-art-2.png');
-  createCanvas(900, 913);
+ // Setting the background as a maze for the user to navigate
+ bg = image(img, 0, 0);
+ createCanvas(900, 913);
 }
 
 // Allowing the user to draw as they navigate to keep track of their movement
 function draw() {
+ // put drawing code here
+}
 // Drawing a black line to make the mark as visible as possible
-  stroke(0);
+ stroke(0);
 // Drawing through clicking and holding so the user can decide whether or not they want to make marks
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
+ if (mouseIsPressed === true) {
+   line(mouseX, mouseY, pmouseX, pmouseY);
+ }
 }
